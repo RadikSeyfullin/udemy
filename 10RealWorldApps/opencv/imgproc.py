53e06c53377243df484f1fce5736b6e77c9d6e59
RadikSeyfullin <radikseyfullin@gmail.com>
2019-09-20 14:00:09 +0300

    Added OpenCV projects (test)

---
+++ 10RealWorldApps/opencv/imgproc.py
@@ -0,0 +1,8 @@
+import cv2, glob
+
+images = glob.glob("*.jpg")
+
+for image in images:
+    img = cv2.imread(image, 1)
+    resized_image = cv2.resize(img, (100, 100))
+    cv2.imwrite("resized_"+image, resized_image)
